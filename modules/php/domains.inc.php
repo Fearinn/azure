2c3922112ad39341a5477505a553594bd2ba43e3
Matheus Gomes <105985983+Fearinn@users.noreply.github.com>
2025-07-25 00:19:16 -0300

    chore: define base material

---
+++ modules/php/domains.inc.php
@@ -0,0 +1,444 @@
+<?php
+
+$this->DOMAINS = [
+    1 => [
+        "sides" => [
+            1 => [
+                1 => [
+                    1 => [
+                        "qi" => 0,
+                        "qi_color" => 0,
+                        "wisdom" => 0,
+                    ],
+                    2 => [
+                        "qi" => 1,
+                        "qi_color" => 4,
+                        "wisdom" => 1,
+                    ],
+                    3 => [
+                        "qi" => 2,
+                        "qi_color" => 3,
+                        "wisdom" => 1,
+                    ],
+                ],
+                2 => [
+                    1 => [
+                        "qi" => 1,
+                        "qi_color" => 2,
+                        "wisdom" => 0,
+                    ],
+                    2 => [
+                        "qi" => 0,
+                        "qi_color" => 0,
+                        "wisdom" => 4,
+                    ],
+                    3 => [
+                        "qi" => 1,
+                        "qi_color" => 2,
+                        "wisdom" => 0,
+                    ],
+                ],
+                3 => [
+                    1 => [
+                        "qi" => 1,
+                        "qi_color" => 2,
+                        "wisdom" => 2,
+                    ],
+                    2 => [
+                        "qi" => 1,
+                        "qi_color" => 3,
+                        "wisdom" => 0,
+                    ],
+                    3 => [
+                        "qi" => 0,
+                        "qi_color" => 0,
+                        "wisdom" => 6,
+                    ],
+                ]
+            ],
+            2 => [
+                1 => [
+                    1 => [
+                        "qi" => 1,
+                        "qi_color" => 4,
+                        "wisdom" => 1,
+                    ],
+                    2 => [
+                        "qi" => 1,
+                        "qi_color" => 2,
+                        "wisdom" => 1,
+                    ],
+                    3 => [
+                        "qi" => 1,
+                        "qi_color" => 3,
+                        "wisdom" => 0,
+                    ],
+                ],
+                2 => [
+                    1 => [
+                        "qi" => 0,
+                        "qi_color" => 0,
+                        "wisdom" => 0,
+                    ],
+                    2 => [
+                        "qi" => 2,
+                        "qi_color" => 1,
+                        "wisdom" => 1,
+                    ],
+                    3 => [
+                        "qi" => 2,
+                        "qi_color" => 3,
+                        "wisdom" => 1,
+                    ],
+                ],
+                3 => [
+                    1 => [
+                        "qi" => 0,
+                        "qi_color" => 0,
+                        "wisdom" => 4,
+                    ],
+                    2 => [
+                        "qi" => 1,
+                        "qi_color" => 2,
+                        "wisdom" => 0,
+                    ],
+                    3 => [
+                        "qi" => 1,
+                        "qi_color" => 2,
+                        "wisdom" => 2,
+                    ],
+                ]
+            ]
+        ],
+    ],
+    2 => [
+        "sides" => [
+            1 => [
+                1 => [
+                    1 => [
+                        "qi" => 0,
+                        "qi_color" => 0,
+                        "wisdom" => 0,
+                    ],
+                    2 => [
+                        "qi" => 1,
+                        "qi_color" => 3,
+                        "wisdom" => 1,
+                    ],
+                    3 => [
+                        "qi" => 0,
+                        "qi_color" => 0,
+                        "wisdom" => 4,
+                    ],
+                ],
+                2 => [
+                    1 => [
+                        "qi" => 1,
+                        "qi_color" => 1,
+                        "wisdom" => 2,
+                    ],
+                    2 => [
+                        "qi" => 1,
+                        "qi_color" => 4,
+                        "wisdom" => 0,
+                    ],
+                    3 => [
+                        "qi" => 1,
+                        "qi_color" => 2,
+                        "wisdom" => 1,
+                    ],
+                ],
+                3 => [
+                    1 => [
+                        "qi" => 1,
+                        "qi_color" => 2,
+                        "wisdom" => 0,
+                    ],
+                    2 => [
+                        "qi" => 0,
+                        "qi_color" => 0,
+                        "wisdom" => 6,
+                    ],
+                    3 => [
+                        "qi" => 2,
+                        "qi_color" => 4,
+                        "wisdom" => 1,
+                    ],
+                ]
+            ],
+            2 => [
+                1 => [
+                    1 => [
+                        "qi" => 1,
+                        "qi_color" => 2,
+                        "wisdom" => 1,
+                    ],
+                    2 => [
+                        "qi" => 2,
+                        "qi_color" => 4,
+                        "wisdom" => 1,
+                    ],
+                    3 => [
+                        "qi" => 0,
+                        "qi_color" => 0,
+                        "wisdom" => 4,
+                    ],
+                ],
+                2 => [
+                    1 => [
+                        "qi" => 0,
+                        "qi_color" => 0,
+                        "wisdom" => 0,
+                    ],
+                    2 => [
+                        "qi" => 0,
+                        "qi_color" => 0,
+                        "wisdom" => 6,
+                    ],
+                    3 => [
+                        "qi" => 1,
+                        "qi_color" => 1,
+                        "wisdom" => 0,
+                    ],
+                ],
+                3 => [
+                    1 => [
+                        "qi" => 1,
+                        "qi_color" => 4,
+                        "wisdom" => 0,
+                    ],
+                    2 => [
+                        "qi" => 1,
+                        "qi_color" => 3,
+                        "wisdom" => 1,
+                    ],
+                    3 => [
+                        "qi" => 1,
+                        "qi_color" => 1,
+                        "wisdom" => 2,
+                    ],
+                ]
+            ]
+        ]
+    ],
+    3 => [
+        "sides" => [
+            1 => [
+                1 => [
+                    1 => [
+                        "qi" => 1,
+                        "qi_color" => 4,
+                        "wisdom" => 2,
+                    ],
+                    2 => [
+                        "qi" => 1,
+                        "qi_color" => 2,
+                        "wisdom" => 1,
+                    ],
+                    3 => [
+                        "qi" => 1,
+                        "qi_color" => 1,
+                        "wisdom" => 1,
+                    ],
+                ],
+                2 => [
+                    1 => [
+                        "qi" => 0,
+                        "qi_color" => 0,
+                        "wisdom" => 0,
+                    ],
+                    2 => [
+                        "qi" => 0,
+                        "qi_color" => 0,
+                        "wisdom" => 6,
+                    ],
+                    3 => [
+                        "qi" => 1,
+                        "qi_color" => 4,
+                        "wisdom" => 0,
+                    ],
+                ],
+                3 => [
+                    1 => [
+                        "qi" => 0,
+                        "qi_color" => 0,
+                        "wisdom" => 4,
+                    ],
+                    2 => [
+                        "qi" => 1,
+                        "qi_color" => 4,
+                        "wisdom" => 1,
+                    ],
+                    3 => [
+                        "qi" => 2,
+                        "qi_color" => 2,
+                        "wisdom" => 1,
+                    ],
+                ]
+            ],
+            2 => [
+                1 => [
+                    1 => [
+                        "qi" => 1,
+                        "qi_color" => 4,
+                        "wisdom" => 2,
+                    ],
+                    2 => [
+                        "qi" => 1,
+                        "qi_color" => 4,
+                        "wisdom" => 0,
+                    ],
+                    3 => [
+                        "qi" => 2,
+                        "qi_color" => 2,
+                        "wisdom" => 1,
+                    ],
+                ],
+                2 => [
+                    1 => [
+                        "qi" => 0,
+                        "qi_color" => 0,
+                        "wisdom" => 4,
+                    ],
+                    2 => [
+                        "qi" => 0,
+                        "qi_color" => 0,
+                        "wisdom" => 0,
+                    ],
+                    3 => [
+                        "qi" => 1,
+                        "qi_color" => 1,
+                        "wisdom" => 1,
+                    ],
+                ],
+                3 => [
+                    1 => [
+                        "qi" => 1,
+                        "qi_color" => 2,
+                        "wisdom" => 1,
+                    ],
+                    2 => [
+                        "qi" => 1,
+                        "qi_color" => 4,
+                        "wisdom" => 1,
+                    ],
+                    3 => [
+                        "qi" => 0,
+                        "qi_color" => 0,
+                        "wisdom" => 6,
+                    ],
+                ]
+            ]
+        ],
+    ],
+    4 => [
+        "sides" => [
+            1 => [
+                1 => [
+                    1 => [
+                        "qi" => 0,
+                        "qi_color" => 0,
+                        "wisdom" => 0,
+                    ],
+                    2 => [
+                        "qi" => 1,
+                        "qi_color" => 3,
+                        "wisdom" => 2,
+                    ],
+                    3 => [
+                        "qi" => 1,
+                        "qi_color" => 1,
+                        "wisdom" => 0,
+                    ],
+                ],
+                2 => [
+                    1 => [
+                        "qi" => 0,
+                        "qi_color" => 0,
+                        "wisdom" => 7,
+                    ],
+                    2 => [
+                        "qi" => 1,
+                        "qi_color" => 2,
+                        "wisdom" => 0,
+                    ],
+                    3 => [
+                        "qi" => 1,
+                        "qi_color" => 3,
+                        "wisdom" => 1,
+                    ],
+                ],
+                3 => [
+                    1 => [
+                        "qi" => 1,
+                        "qi_color" => 3,
+                        "wisdom" => 0,
+                    ],
+                    2 => [
+                        "qi" => 2,
+                        "qi_color" => 1,
+                        "wisdom" => 1,
+                    ],
+                    3 => [
+                        "qi" => 0,
+                        "qi_color" => 0,
+                        "wisdom" => 4,
+                    ],
+                ]
+            ],
+            2 => [
+                1 => [
+                    1 => [
+                        "qi" => 1,
+                        "qi_color" => 1,
+                        "wisdom" => 0,
+                    ],
+                    2 => [
+                        "qi" => 0,
+                        "qi_color" => 0,
+                        "wisdom" => 7,
+                    ],
+                    3 => [
+                        "qi" => 1,
+                        "qi_color" => 3,
+                        "wisdom" => 1,
+                    ],
+                ],
+                2 => [
+                    1 => [
+                        "qi" => 0,
+                        "qi_color" => 0,
+                        "wisdom" => 0,
+                    ],
+                    2 => [
+                        "qi" => 2,
+                        "qi_color" => 1,
+                        "wisdom" => 1,
+                    ],
+                    3 => [
+                        "qi" => 1,
+                        "qi_color" => 3,
+                        "wisdom" => 0,
+                    ],
+                ],
+                3 => [
+                    1 => [
+                        "qi" => 1,
+                        "qi_color" => 3,
+                        "wisdom" => 2,
+                    ],
+                    2 => [
+                        "qi" => 1,
+                        "qi_color" => 2,
+                        "wisdom" => 0,
+                    ],
+                    3 => [
+                        "qi" => 0,
+                        "qi_color" => 0,
+                        "wisdom" => 4,
+                    ],
+                ]
+            ]
+        ],
+    ],
+];
---
+++ modules/php/materials.inc.php
@@ -0,0 +1,31 @@
+<?php
+
+$this->COLORS = [
+    1 => [
+        "label" => clienttranslate("Azure"),
+    ],
+    2 => [
+        "label" => clienttranslate("White"),
+    ],
+    3 => [
+        "label" => clienttranslate("Vermillion"),
+    ],
+    4 => [
+        "label" => clienttranslate("Black"),
+    ]
+];
+
+$this->BEASTS = [
+    1 => [
+        "label" => clienttranslate("Azure Dragon"),
+    ],
+    2 => [
+        "label" => clienttranslate("White Tiger"),
+    ],
+    3 => [
+        "label" => clienttranslate("Vermillion Bird"),
+    ],
+    4 => [
+        "label" => clienttranslate("Black Tortoise"),
+    ],
+];
