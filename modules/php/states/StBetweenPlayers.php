492e594ea2654baa33004ae7a8a49e4446f38858
Matheus Gomes <105985983+Fearinn@users.noreply.github.com>
2025-08-13 20:10:47 -0300

    chore: notify end of turn

--- modules/php/states/StBetweenPlayers.php
+++ modules/php/states/StBetweenPlayers.php
@@ -4,6 +4,7 @@ namespace Bga\Games\Azure\states;
 
 use Bga\Games\Azure\components\Beasts\Bird;
 use Bga\Games\Azure\Game;
+use Bga\Games\Azure\notifications\NotifManager;
 
 class StBetweenPlayers extends StateManager
 {
@@ -15,18 +16,25 @@ class StBetweenPlayers extends StateManager
     public function act(): void
     {
         $pendingBird = $this->globals->get(G_PENDING_BIRD);
+
         if ($pendingBird) {
             $Bird = new Bird($this->game);
             $Bird->execFavor($pendingBird);
-
-            $this->game->gamestate->nextState(TR_NEXT_PLAYER);
-            return;
+            $player_id = $pendingBird;
+        } else {
+            $player_id = (int) $this->game->getActivePlayerId();
+            $this->game->giveExtraTime($player_id);
+            $this->game->azr_activeNextPlayer();
         }
 
-        $player_id = (int) $this->game->getActivePlayerId();
-        $this->game->giveExtraTime($player_id);
+        $Notify = new NotifManager($this->game);
+        $Notify->all(
+            "message",
+            clienttranslate('${player_name} ends his turn'),
+            [],
+            $player_id,
+        );
 
-        $this->game->azr_activeNextPlayer();
         $this->game->gamestate->nextState(TR_NEXT_PLAYER);
     }
 }
